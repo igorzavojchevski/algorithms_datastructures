1e7bb3d415332b0943a6bed6d24de2386e696dd5
igorzavojchevski <igor.zavojcevski@gmail.com>
2023-03-19 00:50:54 +0100

    1762. Buildings With an Ocean View

---
+++ AL_DS/LeetCode/1762-BuildingsWithanOceanView.cs
@@ -0,0 +1,20 @@
+﻿namespace AL_DS.LeetCode
+{
+    public class _1762_BuildingsWithanOceanView
+    {
+        public int[] FindBuildings(int[] heights)
+        {
+            Stack<int> buildings = new Stack<int>();
+
+            for (int i = heights.Length - 1; i >= 0; i--)
+            {
+                if (!buildings.Any() || heights[i] > heights[buildings.Peek()])
+                {
+                    buildings.Push(i);
+                }
+            }
+
+            return buildings.ToArray();
+        }
+    }
+}
