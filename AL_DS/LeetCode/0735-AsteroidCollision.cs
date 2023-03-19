1f5e8317197841bcf3cb43102097427287a19d25
igorzavojchevski <igor.zavojcevski@gmail.com>
2023-03-19 02:41:37 +0100

    735. Asteroid Collision

---
+++ AL_DS/LeetCode/0735-AsteroidCollision.cs
@@ -0,0 +1,33 @@
+﻿namespace AL_DS.LeetCode
+{
+    public class _0735_AsteroidCollision
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> ans = new Stack<int>();
+            for (int i = 0; i < asteroids.Length; i++)
+            {
+                if (ans.Count == 0 || asteroids[i] > 0)
+                {
+                    ans.Push(asteroids[i]);
+                }
+                else
+                {
+                    while (ans.Count != 0 && ans.Peek() > 0 && ans.Peek() < Math.Abs(asteroids[i]))
+                    {
+                        ans.Pop();
+                    }
+                    if (ans.Count != 0 && ans.Peek() == Math.Abs(asteroids[i]))
+                    {
+                        ans.Pop();
+                    }
+                    else if (ans.Count == 0 || ans.Peek() < 0)
+                    {
+                        ans.Push(asteroids[i]);
+                    }
+                }
+            }
+            return ans.ToArray().Reverse().ToArray();
+        }
+    }
+}
